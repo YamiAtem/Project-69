3dce645b8d7e70fca75d8c227e4baa6bc94540cc
Atharva Mishra <atharvamishra956@gmail.com>
2021-02-20 09:51:04 +0530

    created app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View } from 'react-native';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+import ScanScreen from './screens/ScanScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() { 
+    return (
+      <View>
+        <AppContainer/>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+var AppNavigator = createSwitchNavigator({
+  ScanScreen:ScanScreen
 });
+
+const AppContainer = createAppContainer(AppNavigator);
